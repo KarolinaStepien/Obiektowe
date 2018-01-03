3ef0fdad88e0851f548853afd72110fc59dcdcb3
KarolinaStepien <kastepien@student.agh.edu.pl>
2018-01-04 00:20:32 +0100

    ChristmasTreeMainClassUpdate

--- src/lab6/Main.java
+++ src/lab6/Main.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
         JFrame frame = new JFrame("Choinka");
         frame.setContentPane(new DrawPanel());
         frame.setSize(1000, 700);
--- src/lab7/ClockWithGui.java
+++ src/lab7/ClockWithGui.java
@@ -1,4 +1,45 @@
 package lab7;
 
-public class ClockWithGui {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
+import java.time.LocalTime;
+
+public class ClockWithGui extends JPanel {
+
+    LocalTime time = LocalTime.now();
+
+    public void paintComponent(Graphics g){
+        Graphics2D g2d=(Graphics2D)g;
+        g2d.translate(getWidth()/2,getHeight()/2);
+
+        //tarcza zegara
+        for(int i=1;i<13;i++){
+            AffineTransform at = new AffineTransform();
+            at.rotate(2*Math.PI/12*i);
+            Point2D src = new Point2D.Float(0,-120);
+            Point2D trg = new Point2D.Float();
+            at.transform(src,trg);
+            g2d.drawString(Integer.toString(i),(int)trg.getX(),(int)trg.getY());
+        }
+
+        //wskazowki
+        AffineTransform saveAT = g2d.getTransform();
+        g2d.rotate(time.getHour()%12*2*Math.PI/12);
+        g2d.drawLine(0,0,0,-100);
+        g2d.setTransform(saveAT);
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Clock");
+        frame.setContentPane(new ClockWithGui());
+        frame.setSize(700, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setVisible(true);
+
+    }
 }
